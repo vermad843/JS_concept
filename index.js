8dd058e5f2b8e1a6bb0c6019300bd6d63719ee5d
vermad843 <vishnuverma7524@gmail.com>
2022-05-03 04:29:53 +0530

    mathematical operations

---
+++ index.js
@@ -0,0 +1,16 @@
+// Mathematical operations
+
+let firstNumber = 5;
+let secondNumber = 7;
+
+let add = firstNumber + secondNumber;
+console.log(add);
+
+let subtract = firstNumber - secondNumber;
+console.log(subtract);
+
+let multiply = firstNumber * secondNumber;
+console.log(multiply);
+
+let divide = firstNumber/secondNumber;
+console.log(divide);
